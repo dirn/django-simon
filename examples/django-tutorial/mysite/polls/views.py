60ae06cdd3bc20015bae73c9c65c6d621a8a1b94
Andy Dirnberger <dirn@dirnonline.com>
2013-02-07 10:40:27 -0500

    Add date checking to tutorial example
    This change is introduced in part 5 of the Django tutorial. It is controlled through the generic views being controlled in the `urls` module. Because generic views do not work with django-simon models, the work is being done in `views.py`.

--- examples/django-tutorial/mysite/polls/views.py
+++ examples/django-tutorial/mysite/polls/views.py
@@ -1,29 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.utils import timezone
 from django_simon import get_object_or_404
 
 from polls.models import Poll
 
 
 def index(request):
-    latest_poll_list = Poll.all().sort('-pub_date').limit(5)
+    latest_poll_list = Poll.find(pub_date__lte=timezone.now()) \
+        .sort('-pub_date').limit(5)
     context = {'latest_poll_list': latest_poll_list}
     return render(request, 'polls/index.html', context)
 
 
 def detail(request, poll_id):
-    poll = get_object_or_404(Poll, id=poll_id)
+    poll = get_object_or_404(Poll, id=poll_id, pub_date__lte=timezone.now())
     return render(request, 'polls/detail.html', {'poll': poll})
 
 
 def results(request, poll_id):
-    poll = get_object_or_404(Poll, id=poll_id)
+    poll = get_object_or_404(Poll, id=poll_id, pub_date__lte=timezone.now())
     return render(request, 'polls/results.html', {'poll': poll})
 
 
 def vote(request, poll_id):
-    p = get_object_or_404(Poll, id=poll_id)
+    p = get_object_or_404(Poll, id=poll_id, pub_date__lte=timezone.now())
 
     if 'choice' in request.POST:
         index = 0
@@ -36,4 +38,3 @@ def vote(request, poll_id):
         'poll': p,
         'error_message': "You didn't select a choice.",
     })
-
